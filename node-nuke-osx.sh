b4bdaee21745a3e57dcc83b81c38ea1dcddb3f7b
Alistair MacDonald <al@pwn.io>
2016-01-20 11:18:32 -0500

    Add splash to terminal.

--- node-nuke-osx.sh
+++ node-nuke-osx.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
 
-## NODE NUKE OSX
-#               .           .   ________________    .        .
-#                     .    ____/ (  (    )   )  \___
-#               .         /( (  (  )   _    ))  )   )\        .   .
-#                       ((     (   )(    )  )   (   )  )   .
-#            .    .   ((/  ( _(   )   (   _) ) (  () )  )        .   .
-#                    ( (  ( (_)   ((    (   )  .((_ ) .  )_
-#         _  _  ___  ___  ___     _  _ _   _ _  _____      ___  _____  __
-#        | \| |/ _ \|   \| __|   | \| | | | | |/ / __|    / _ \/ __\ \/ /
-#        | .` | (_) | |) | _|    | .` | |_| | ' <| _|    | (_) \__ \>  < 
-#        |_|\_|\___/|___/|___|   |_|\_|\___/|_|\_\___|    \___/|___/_/\_\
-#                     _        _  _ _     _       .   .   .
-#     .       .     (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)   .
-#         .         ((__)        \\||lll|l||///          \_))       .
-#                  .       . / (  |(||(|)|||//  \     .    .      .      .
-#    .       .           .   (   /(/ (  )  ) )\          .     .
-#        .      .    .     (  . ( ( ( | | ) ) )\   )               .
-#                           (   /(| / ( )) ) ) )) )    .   .  .       .  .  .
-#    .     .       .  .   (  .  ( ((((_(|)_)))))     )            .
-#            .  .          (    . ||\(|(|)|/|| . . )        .        .
-#        .           .   (   .    |(||(||)||||   .    ) .      .         .  .
-#    .      .      .       (     //|/l|||)|\\ \     )      .      .   .
-#                        (/ / //  /|//||||\\  \ \  \ _)
-#----------------------------------------------------------------------------
+SPLASH=(
+"## NODE NUKE OSX                                                            "
+"               .           .   ________________    .        .               "
+"                     .    ____/ (  (    )   )  \___                         "
+"               .         /( (  (  )   _    ))  )   )\        .   .          "
+"                       ((     (   )(    )  )   (   )  )   .                 "
+"            .    .   ((/  ( _(   )   (   _) ) (  () )  )        .   .       "
+"                    ( (  ( (_)   ((    (   )  .((_ ) .  )_                  "
+"         _  _  ___  ___  ___     _  _ _   _ _  _____      ___  _____  __    "
+"        | \| |/ _ \|   \| __|   | \| | | | | |/ / __|    / _ \/ __\ \/ /    "
+"        | .\` | (_) | |) | _|    | .\` | |_| | ' <| _|    | (_) \__ \>  <   "
+"        |_|\_|\___/|___/|___|   |_|\_|\___/|_|\_\___|    \___/|___/_/\_\    "
+"                     _        _  _ _     _       .   .   .                  "
+"     .       .     (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)   .            "
+"         .         ((__)        \\\\||lll|l||///          \\_))       .     "
+"                  .       . / (  |(||(|)|||//  \\     .    .      .      .  "
+"    .       .           .   (   /(/ (  )  ) )\\          .     .            "
+"        .      .    .     (  . ( ( ( | | ) ) )\\   )               .        "
+"                           (   /(| / ( )) ) ) )) )    .   .  .       .  .  ."
+"    .     .       .  .   (  .  ( ((((_(|)_)))))     )            .          "
+"            .  .          (    . ||\\(|(|)|/|| . . )        .        .      "
+"        .           .   (   .    |(||(||)||||   .    ) .      .         .  ."
+"    .      .      .       (     //|/l|||)|\\\\ \\     )      .      .   .   "
+"                        (/ / //  /|//||||\\\\  \ \\  \\ _)                  "
+"----------------------------------------------------------------------------"
+)
 
 NodeDirectories=(
 
-  # Default Installation
+  # Default Installations
   /usr/local/lib/node
   /usr/local/lib/node_modules
+  /usr/local/include/node
+  /usr/local/include/node_modules
 
   # Should by removed by "brew uninstall"
   /usr/local/lib/node_modules
@@ -48,6 +52,9 @@ NodeDirectories=(
 
   # Node Version Manager
   ~/.nvm
+
+  # Opt
+  /opt/local/lib/node_modules
 )
 
 NodeFiles=(
@@ -55,40 +62,49 @@ NodeFiles=(
   # Symbolic links
   /usr/local/bin/npm
   /usr/local/bin/node
-  /usr/local/lib/dtrace/node.d  # Should be removed by "brew uninstall"
+
+  # Should be removed by "brew uninstall"
+  /usr/local/lib/dtrace/node.d
+  /usr/local/share/systemtap/tapset/node.stp
+  /usr/local/share/man/man1/node.1
+  
+  # Opt
+  /opt/local/bin/node
+  /opt/local/include/node
 )
 
 
+for line in "${SPLASH[@]}"
+do
+  echo "$line"
+done
 
-# CMD="echo sudo rm -fr"
-# CMD="ls -lRd"
-CMD="find"
-CMD=""
-POSTFIX=""
+
 MODE="DRYRUN"
-VERBOSE=false
+LIST=false
 ARGS_FILE="-f"
 ARGS_DIR="-rf"
 ARGS_VERBOSE="v"
 
-while getopts "Dvh" arg; do
+
+while getopts "Dlh" arg; do
   case $arg in
     D)
-      # Override dry-run with actual remove command
-      # CMD="sudo rm -vfr"
+      # Delete each file/directory (overrides dry-run)
       MODE="NUKE"
       ;;
-    v)
-      # Verbose output, lists all files
-      VERBOSE=true
+    l)
+      # List each file/directory that would be deleted
+      LIST=true
       ARGS_FILE=$ARGS_FILE$ARGS_VERBOSE
       ARGS_DIR=$ARGS_DIR$ARGS_VERBOSE 
       ;;
     h)
+      # Display help and exit
       echo 'Help: Node Nuke OSX'
-      echo '  v - Verbose list of all files and directories to be removed'
-      echo '  h - show help list (you are reading it now)'
-      echo '  D - delete all node.js files and directories'
+      echo '  l - List each file/directory that would be deleted'
+      echo '  h - Show help (you are reading help now)'
+      echo '  D - Delete each node.js file and directorie'
       exit
       ;;
     esac
@@ -97,6 +113,7 @@ done
 
 (( ${#} > 0 )) || {
   echo 'DISCLAIMER: USE THIS SCRIPT AT YOUR OWN RISK!'
+  echo
   echo 'THE AUTHOR TAKES NO RESPONSIBILITY FOR THE RESULTS OF THIS SCRIPT.'
   echo
   echo 'Node Nuke OSX will attempt to remove all traces of Node.JS from,'
@@ -111,15 +128,21 @@ done
   echo '[CTRL-C] Cancel, [ENTER] Continue'
   read
   echo 'This script requires admin priviledges.'
+  echo
   echo 'You may be prompted for a password.'
+  echo
   sudo ${0} sudo
   exit $?
 }
 
 
+if [ $MODE = "DRYRUN" ]; then
+  echo
+  echo "BOM Packages marked for deletion: \n"
+fi
+
 
 # This will need to be executed as an Admin (maybe just use sudo).
-
 for bom in org.nodejs.node.pkg.bom org.nodejs.pkg.bom; do
 
   receipt=/var/db/receipts/${bom}
@@ -129,7 +152,7 @@ for bom in org.nodejs.node.pkg.bom org.nodejs.pkg.bom; do
     | while read i; do
       # Remove each file listed in the bom.
       if [ $MODE = "DRYRUN" ]; then
-        echo /usr/local/${i} $POSTFIX
+        echo /usr/local/${i}
       else
         sudo rm $ARGS_DIR /usr/local/${i}
       fi
@@ -146,10 +169,12 @@ fi
 
 for dir in "${NodeDirectories[@]}"
 do
+  # If the directory exists
   if [ -d "$dir" ]; then
+    # 
     if [ $VERBOSE = true ]; then
       find $dir -type d -o -type f -o -type l;
-    else 
+    else
       echo "  $dir"
     fi
     if [ $MODE = "NUKE" ]; then
@@ -184,3 +209,9 @@ fi
 
 
 exit 0
+
+# Credits:
+#   @nicerobot - https://gist.github.com/ddo/668630454ea0d74fdc21
+#   StackOverflow - http://stackoverflow.com/questions/9044788/how-do-i-uninstall-nodejs-installed-from-pkg-mac-os-x
+#   @benznext - http://benznext.com/completely-uninstall-node-js-from-mac-os-x/
+
